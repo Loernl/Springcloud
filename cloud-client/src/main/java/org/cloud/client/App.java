2438a0cc213ae3d69665b7c188a9be504973de52
lg99 <Lola.L.Gou@newegg.com>
2018-08-25 16:20:17 +0800

    first commit

---
+++ cloud-client/src/main/java/org/cloud/client/App.java
@@ -0,0 +1,27 @@
+package org.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+    @GetMapping("/service") 
+    public String service(){
+    	return "service";
+    }
+}
